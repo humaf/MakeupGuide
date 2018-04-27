1d20d96824487bf7a005d770f985b86fcf6856e2
humaf <qfhuma@gmail.com>
2018-04-26 22:12:14 -0700

    first commit

---
+++ app/src/main/java/strawbericreations/com/makeupguide/userinterface/VideosActivity.java
@@ -0,0 +1,14 @@
+package strawbericreations.com.makeupguide.userinterface;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class VideosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videos);
+    }
+}

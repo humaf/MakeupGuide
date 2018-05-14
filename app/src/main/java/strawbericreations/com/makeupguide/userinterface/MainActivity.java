6b5a3c2289f3c72f22b6bc3f28910a1fd9035926
humaf <qfhuma@gmail.com>
2018-05-13 23:54:24 -0700

    fix:Correct data displaying in recycler view but need to fix the display

--- app/src/main/java/strawbericreations/com/makeupguide/userinterface/MainActivity.java
+++ app/src/main/java/strawbericreations/com/makeupguide/userinterface/MainActivity.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package strawbericreations.com.makeupguide.userinterface;
 
 import android.content.Intent;
@@ -35,42 +35,3 @@ public class MainActivity extends AppCompatActivity {
         startActivity(dataIntent);
     }
 }
-||||||| merged common ancestors
-=======
-package strawbericreations.com.makeupguide.userinterface;
-
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-
-import strawbericreations.com.makeupguide.R;
-
-public class MainActivity extends AppCompatActivity {
-
-    Intent dataIntent;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        dataIntent = new Intent(this,VideosActivity.class);
-    }
-
-    public void eyesClick(View view){
-        dataIntent.putExtra("keyword","eyemakeup");
-        startActivity(dataIntent);
-
-    }
-
-    public void faceClick(View view){
-        dataIntent.putExtra("keyword","facemakeup");
-        startActivity(dataIntent);
-    }
-
-    public void lipClick(View view){
-        dataIntent.putExtra("keyword","lipsmakeup");
-        startActivity(dataIntent);
-    }
-}
->>>>>>> c501a30b4325b578475ffab73abe70e3b7811a45

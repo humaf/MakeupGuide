e59627a9987f6fde319f9a149a47383dfed4e958
humaf <qfhuma@gmail.com>
2018-05-30 01:11:55 -0700

    Feat:Intgerated Analytics and AdMob

---
+++ app/src/main/java/strawbericreations/com/makeupguide/analytics/AnalyticsApplication.java
@@ -0,0 +1,40 @@
+package strawbericreations.com.makeupguide.analytics;
+
+import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+import strawbericreations.com.makeupguide.R;
+
+/**
+ * Created by redrose on 5/29/18.
+ */
+
+public class AnalyticsApplication extends Application {
+
+    private static GoogleAnalytics sAnalytics;
+    private static Tracker sTracker;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        sAnalytics = GoogleAnalytics.getInstance(this);
+    }
+
+    /**
+     * Gets the default {@link Tracker} for this {@link Application}.
+     * @return tracker
+     */
+    synchronized public Tracker getDefaultTracker() {
+        // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+        if (sTracker == null) {
+            sTracker = sAnalytics.newTracker(R.xml.global_tracker);
+        }
+
+        return sTracker;
+    }
+
+
+
+}
--- app/src/main/java/strawbericreations/com/makeupguide/userinterface/MainActivity.java
+++ app/src/main/java/strawbericreations/com/makeupguide/userinterface/MainActivity.java
@@ -4,20 +4,48 @@ package strawbericreations.com.makeupguide.userinterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
 import strawbericreations.com.makeupguide.R;
+import strawbericreations.com.makeupguide.analytics.AnalyticsApplication;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity" ;
     Intent dataIntent;
+
+    private Tracker mTracker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AnalyticsApplication application = (AnalyticsApplication) getApplication();
+        mTracker = application.getDefaultTracker();
+
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544/6300978111");
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest request = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)        // All emulators
+                // An example device ID
+                .build();
+        mAdView.loadAd(request);
+
+
         dataIntent = new Intent(this, VideoActivity.class);
     }
+
+
 
     public void eyesClick(View view) {
         dataIntent.putExtra("keyword", "eyemakeup");
